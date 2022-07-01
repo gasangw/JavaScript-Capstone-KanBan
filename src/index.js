e2833c59da1ffe7b9bb58aad724571acc1301b15
Gasangwa Thomas <thomasgasangwa@gmail.com>
2022-07-01 08:02:42 +0200

    Display cards using API and style them

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
 
-/* import {
-  postLikes, fetchData, updateLikes, postComment,
-} from './modules/getShowsList.js';
+import fetchData from './modules/getShowsList.js';
 
 fetchData();
-postComment();
+/* postComment();
 postLikes();
 updateLikes(); */
